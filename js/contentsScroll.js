2260f56cc59f59a005983641b1d273619f5b68a7
nox590276 <nox590276@naver.com>
2023-08-25 15:21:12 +0900

    business_performance 경로수정 및 css파일 생성

--- js/contentsScroll.js
+++ js/contentsScroll.js
@@ -1,40 +1,17 @@
 window.addEventListener('load', () => {
   const $contents = document.querySelectorAll('#main_contents>div')
   const $businessSection = document.querySelectorAll('#business_section>li')
-  const $headerWrap = document.querySelector('#header_wrap')
-  const $mainMenu=document.querySelectorAll('#mainmenu_list>li>a');
-  const $logo= document.querySelector('#logo>span')
-  const $subMenuList = document.querySelectorAll('.submenu_list');
+  
 
   //  window.addEventListener('mousewheel',wheelWindow)
   window.addEventListener('scroll', scrollEvent)
-  window.addEventListener('scroll', scrollEventMenu)
+ 
 
   let windowHeight = window.innerHeight;
   let clickIndex = 0;
   let slideLength = $contents.length;
   let isWheel = false;
   let scrollposition = null;
-  let closeHeight = 120;
-
-
-  function scrollEventMenu() {
-    scrollposition = window.pageYOffset;
-    if (scrollposition >= 30) {
-      gsap.to($headerWrap, { backgroundColor: 'white', duration: 0.5 });
-      for (let item of $mainMenu) {
-        gsap.to(item, { color: '#1B2040', duration: 0.5 });
-      }
-      gsap.to($logo,{color :'#1B2040'})
-      gsap.to($subMenuList,{hegiht:closeHeight})
-    } else {
-      gsap.to($headerWrap, { backgroundColor: 'transparent', duration: 0.5 });
-      for (let item of $mainMenu) {
-        gsap.to(item, { color: 'white', duration: 0.5 });
-      }
-      gsap.to($logo,{color :'white'})
-    }
-  }
 
   function activateMainMenu(index) {
     if (selectedMenu != null && selectedMenu != $mainMenu[index]) {
